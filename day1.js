8be79afd8023f9fc7d95c5e86b336da64cfeb3b6
Tonni Akter <tonniakterbithi@gmaile.com>
2021-11-28 20:59:21 +0600

    a

---
+++ day1.js
@@ -0,0 +1,20 @@
+// document.getElementById("demo").style.fontSize = "50px";
+
+var pi = 3.1416;
+var person = "Tonni Akter";
+
+var carName;
+carname="marcedes"
+console.log(carname);
+var x = 2 + 3 + "5";
+console.log(x);
+
+/* The let keyword was introduced in ES6 (2015).
+
+Variables defined with let cannot be Redeclared.
+
+Variables defined with let must be Declared before use.
+
+Variables defined with let have Block Scope. 
+let and const both are block scope variable
+*/
